9cfd51c799a2c78769c54970d0bf99d825d61136
Ankit <ankit.escalate360@gmail.com>
2020-09-24 22:43:53 +0530

    product page

--- frontend/src/components/ProductPage.js
+++ frontend/src/components/ProductPage.js
@@ -1,9 +1,57 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-const ProductPage = () => {
+import { Link, useParams } from 'react-router-dom';
+import data from '../data';
+const ProductPage = (props) => {
   let { id } = useParams();
-  console.log(id);
-  return <div>Product Page</div>;
+
+  const product = data.products.find((x) => x._id == id);
+
+  return (
+    <div className="details">
+      <div className="back-to-result">
+        <Link to="/">Back to results</Link>
+      </div>
+
+      <div className="details-image">
+        <img src={product.image} alt="img-product" />
+      </div>
+      <div className="details-info">
+        <ul>
+          <li>
+            <h4>{product.name}</h4>
+          </li>
+          <li>
+            {product.rating} Stars ({product.numReviews} Reviews)
+          </li>
+          <li>
+            Price: <b>${product.price}</b>
+          </li>
+          <li>
+            Description:
+            <div>{product.description}</div>
+          </li>
+        </ul>
+      </div>
+      <div className="details-action">
+        <ul>
+          <li>Price: {product.price}</li>
+          <li>Status: {product.status}</li>
+          <li>
+            Qty:{' '}
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </li>
+          <li>
+            <button className="button button-primary">Add to cart</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
